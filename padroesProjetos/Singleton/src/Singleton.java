dea3ef4c78b0ad4d975751d949607c2ca759b9a6
Pedro Lucas Rodrigues <101150701+Pedrolucasrd@users.noreply.github.com>
2023-06-12 17:45:50 -0300

    Add files via upload

---
+++ padroesProjetos/Singleton/src/Singleton.java
@@ -0,0 +1,21 @@
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Mensagem do Singleton");
+    }
+}
